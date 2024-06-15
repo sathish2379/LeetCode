65f6c195ebc8f0e5a1f75b9b37c46950f69746e8
sathish2379 <sathishreddygathpa@gmail.com>
2024-06-15 12:50:39 -0700

    LongestCommonSubsequence

---
+++ DynamicProgramming/LongestCommonSubsequence.py
@@ -0,0 +1,40 @@
+'''
+1143. Longest Common Subsequence
+Medium
+
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+'''
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        rows = len(text1)+1
+        columns = len(text2)+1
+        dp = [[0]*columns for _ in range(0, rows)]
+
+        for i in range(1, rows):
+            for j in range(1, columns):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return dp[-1][-1]
