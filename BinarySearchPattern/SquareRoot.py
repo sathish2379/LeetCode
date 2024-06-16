703f61e02c57266e5cbedad86ddd635115ecccc2
sathish2379 <sathishreddygathpa@gmail.com>
2024-06-16 11:06:35 -0700

    Square Root

---
+++ BinarySearchPattern/SquareRoot.py
@@ -0,0 +1,31 @@
+'''
+69. Sqrt(x)
+Easy
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+You must not use any built-in exponent function or operator.
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+Example 1:
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+'''
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 1, x
+        if x == 1:
+            return 1
+
+        while left < right:
+            mid = left + (right - left)//2
+            if mid*mid>x:
+                right = mid
+            else:
+                left = mid+1
+        return left-1
