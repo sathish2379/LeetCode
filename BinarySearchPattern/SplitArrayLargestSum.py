46433e0332da2665d7a0e2ca7e01fff15a24d556
sathish2379 <sathishreddygathpa@gmail.com>
2024-06-16 12:58:29 -0700

    Split Array Largest Sum

---
+++ BinarySearchPattern/SplitArrayLargestSum.py
@@ -0,0 +1,45 @@
+'''
+401: Split Array Largest Sum
+Hard
+
+Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+Return the minimized largest sum of the split.
+A subarray is a contiguous part of the array.
+
+Example 1:
+Input: nums = [7,2,5,10,8], k = 2
+Output: 18
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8], where the largest sum among the two subarrays is only 18.
+Example 2:
+
+Input: nums = [1,2,3,4,5], k = 2
+Output: 9
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [1,2,3] and [4,5], where the largest sum among the two subarrays is only 9.
+'''
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def isSumFeasible(sum: int) -> bool:
+            count = 1 #number of subarrays
+            total = 0
+            for num in nums:
+                total += num
+                if total > sum:
+                    total = num
+                    count +=1
+                    if count > k:
+                        return False
+            return True
+
+        
+        left, right = max(nums), sum(nums)
+        while left < right:
+            mid = left + (right - left)//2
+            if isSumFeasible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+        
