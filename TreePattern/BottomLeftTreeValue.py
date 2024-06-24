cef85d44ab9ef69b0702f0eb5b90b9c13d25711e
sathish2379 <sathishreddygathpa@gmail.com>
2024-06-23 21:25:10 -0700

    Bottom Left tree value

---
+++ TreePattern/BottomLeftTreeValue.py
@@ -0,0 +1,40 @@
+'''
+513. Find Bottom Left Tree Value
+Medium
+
+Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+Example 1:
+Input: root = [2,1,3]
+Output: 1
+
+Example 2:
+Input: root = [1,2,3,4,null,5,6,null,null,7]
+Output: 7
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return None
+        
+        #result = [] for left side view elments 
+        result = None
+        queue = [root]
+        while queue:
+            result = (queue[0].val)
+            size = len(queue)
+            for _ in range(size):
+                node = queue.pop(0)
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+        return result
