e78a7f601648356384d219fb01ab6eaf1ed0b197
sathish2379 <sathishreddygathpa@gmail.com>
2024-06-23 21:01:01 -0700

    symmetric tree

---
+++ TreePattern/SymmetricTree.py
@@ -0,0 +1,33 @@
+'''
+101. Symmetric Tree
+Easy
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        return self.dfs(root.left, root.right)
+    
+    def dfs(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        return p.val == q.val and self.dfs(p.right, q.left) and self.dfs(p.left, q.right) 
