06adbecc5a7873fd0b08ebe031187477748f79bc
sathish2379 <sathishreddygathpa@gmail.com>
2024-07-07 13:59:47 -0700

    Valid Anagram

---
+++ Blind75/ValidAnagram.py
@@ -0,0 +1,43 @@
+'''
+242. Valid Anagram
+Easy
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        hash_s = {}
+        hash_t = {}
+        for i in range(0, len(s)):
+            if s[i] in hash_s:
+                hash_s[s[i]] += 1
+            else:
+                hash_s[s[i]] = 1
+
+            if t[i] in hash_t:
+                hash_t[t[i]] += 1
+            else:
+                hash_t[t[i]] = 1
+        # sorted_s = {key: hash_s[key] for key in sorted(hash_s)}
+        # sorted_t = {key: hash_t[key] for key in sorted(hash_t)}
+        # if sorted_s == sorted_t:
+        #     return True
+        ########### Sorting is not necessary to compare hash tables ################
+        if hash_s == hash_t:
+            return True
+        return False
+
+        ############ we can simply use collections.counter for counting hashable elements in a string #########
+        
