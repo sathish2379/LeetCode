5ca39c1e945e1cef7b928ab76bdd9f190edb5ea7
sathish2379 <sathishreddygathpa@gmail.com>
2024-07-20 16:50:55 -0700

    Insert Interval

---
+++ Blind75/InsertInterval.py
@@ -0,0 +1,32 @@
+'''
+57. Insert Interval
+Medium
+
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+Return intervals after the insertion.
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+'''
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals = sorted(intervals, key = lambda x:x[0])
+        merged = []
+        for i in range(0, len(intervals)):
+            if not merged or merged[-1][1] < intervals[i][0]:
+                #no overlap
+                merged.append(intervals[i])
+            else:
+                merged[-1][1] = max(merged[-1][1], intervals[i][1])
+        return merged
+
+        
