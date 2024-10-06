c0f0719bd37a1fa878a4603bf9929a9efb89267c
sathish2379 <sathishreddygathpa@gmail.com>
2024-10-06 16:42:53 -0700

    Word Pattern

---
+++ Top150/WordPattern.py
@@ -0,0 +1,48 @@
+'''
+290. Word Pattern
+Easy
+
+Given a pattern and a string s, find if s follows the same pattern.
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
+Each letter in pattern maps to exactly one unique word in s.
+Each unique word in s maps to exactly one letter in pattern.
+No two letters map to the same word, and no two words map to the same letter.
+
+Example 1:
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Explanation:
+The bijection can be established as:
+'a' maps to "dog".
+'b' maps to "cat".
+
+Example 2:
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+
+Example 3:
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+'''
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        map_char = {}
+        map_word = {}
+
+        words = s.split(' ')
+        if len(pattern) != len(words):
+            return False
+        for c,w in zip(pattern, words):
+            if c not in map_char:
+                if w in map_word:
+                    return False
+                else:
+                    map_char[c] = w
+                    map_word[w] = c
+            else:
+                if map_char[c] != w:
+                    return False
+                if map_word[w] != c:
+                    return False
+        return True
+        
