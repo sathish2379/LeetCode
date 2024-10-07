194532211b77018cd5a68d1bbdfcdf483f333348
sathish2379 <sathishreddygathpa@gmail.com>
2024-10-06 17:16:40 -0700

    Happy Number Important

--- Top150/HappyNumber.py
+++ Top150/HappyNumber.py
@@ -28,7 +28,7 @@ class Solution:
         def getNext(n):
             total_sum = 0
             while n > 0:
-                n,digit = divmod(n, 10)
+                n,digit = divmod(n, 10) # n//10 and n%10
                 total_sum += digit**2
             return total_sum
 
